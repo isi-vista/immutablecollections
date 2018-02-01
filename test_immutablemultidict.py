9d9d7824bb0b64746e57c03b8bba1f67f6c998ba
Ryan Gabbard <gabbard@isi.edu>
2018-01-31 19:38:13 -0500

    Fix all unit tests except unchained

--- test_immutablemultidict.py
+++ test_immutablemultidict.py
@@ -13,3 +13,22 @@ class TestImmutableMultiDict(TestCase):
         self.assertEqual("{1: {2, 3}, 4: {5, 6}}",
                          str(ImmutableSetMultiDict.of(
                              {1: [2, 3], 4: [5, 6]})))
+
+    def test_unmodified_copy_builder(self):
+        ref: ImmutableSetMultiDict[str, int] = (ImmutableSetMultiDict.builder()
+                                                .put('foo', 5).put('bar', 6)
+                                                .put('foo', 4).build())
+
+        self.assertEqual(ref, ref.modified_copy_builder().build())
+
+    def test_modified_copy_builder(self):
+        start: ImmutableSetMultiDict[str, int] = (ImmutableSetMultiDict.builder()
+                                                  .put('foo', 5).put('bar', 6)
+                                                  .put('foo', 4).build())
+        updated = start.modified_copy_builder().put('bar', 1).put('foo', 7).put('meep', -1).build()
+
+        ref: ImmutableSetMultiDict[str, int] = (ImmutableSetMultiDict.builder()
+                                                .put('foo', 5).put('bar', 6)
+                                                .put('foo', 4).put('foo', 7)
+                                                .put('bar', 1).put('meep', -1).build())
+        self.assertEqual(ref, updated)
