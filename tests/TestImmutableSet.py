7b329171ea296ae375e7970e1fb99a1935556d7a
Ryan Gabbard <gabbard@isi.edu>
2018-08-22 23:14:32 -0400

    Add test that empty immutable set is a singleton

--- tests/TestImmutableSet.py
+++ tests/TestImmutableSet.py
@@ -17,3 +17,14 @@ class TestImmutableSet(TestCase):
         built = builder.build()
         self.assertEqual("hallo", built[0])
         self.assertEqual("hello", built[1])
+
+    def test_empty_is_singleton(self):
+        builder1 = immutablesetbuilder()
+        set1 = builder1.build()
+        builder2 = immutablesetbuilder()
+        set2 = builder2.build()
+        set3 = immutableset([])
+        set4 = immutableset([])
+        self.assertIs(set1, set2)
+        self.assertIs(set2, set3)
+        self.assertIs(set3, set4)
